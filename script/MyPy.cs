a73ce3939b93a71b80664d0965d7adccc4c02a18
小林家的龙码农 <wwz2832918775@163.com>
2026-01-26 16:35:49 +0800

    代码路径config

--- script/MyPy.cs
+++ script/MyPy.cs
@@ -1,20 +1,36 @@
 using System;
 using Godot;
 using Python.Runtime;
+using Newtonsoft.Json;
+using System.IO;
 using Environment = System.Environment;
+
+class Config
+{
+    public string pyRoot { get; set; }
+    public string envRoot { get; set; }
+    public string PythonDLL { get; set; }
+}
 
 class MyPy
 {
     public dynamic sys;
-
     public dynamic api;
+    public string pyRoot;
+    public string envRoot;
+    public string pythonDLL;
 
     public MyPy()
     {
-        var pyRoot = @"D:\project\handwrite\py";
+        // 读取配置，支持注释
+        var configPath = "config/config.json";
+        var configText = File.ReadAllText(configPath);
+        var config = JsonConvert.DeserializeObject<Config>(configText);
+        pyRoot = config.pyRoot;
+        envRoot = config.envRoot;
+        pythonDLL = config.PythonDLL;
 
         // set environment variables
-        var envRoot = @"C:\Users\28329\miniconda3\envs\handwrite";
         var path = Environment.GetEnvironmentVariable("PATH").TrimEnd(';');
         path = string.IsNullOrEmpty(path) ? envRoot : path + ";" + envRoot;
         Environment.SetEnvironmentVariable("PATH", path);
@@ -22,7 +38,7 @@ class MyPy
         Environment.SetEnvironmentVariable("PYTHONPATH", $"{envRoot}\\Lib");
 
         // C:\Users\28329\miniconda3\envs\handwrite\python.exe
-        Runtime.PythonDLL = @"C:\Users\28329\miniconda3\envs\handwrite\python310.dll";
+        Runtime.PythonDLL = pythonDLL;
         PythonEngine.Initialize();
         PythonEngine.PythonHome = envRoot;
         PythonEngine.PythonPath = Environment.GetEnvironmentVariable("PYTHONPATH");
